51daf21e2721821e2440ab4678d4225b242c0b85
evanlee2048 <evanlee2048@gmail.com>
2016-02-02 13:32:42 +0800

    Trial for sound recording

--- app/src/main/java/com/indielink/indielink/AudioRecorderFragment.java
+++ app/src/main/java/com/indielink/indielink/AudioRecorderFragment.java
@@ -1,15 +1,27 @@
 package com.indielink.indielink;
 
+import android.media.MediaRecorder;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+
+import java.io.IOException;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class AudioRecorderFragment extends Fragment {
+
+    private static final String LOG_TAG = "AudioRecordTest";
+    private static String mFileName = null;
+    private MediaRecorder mRecorder = null;
 
     public AudioRecorderFragment() {
     }
@@ -17,6 +29,47 @@ public class AudioRecorderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_audio_recorder, container, false);
+        View view = inflater.inflate(R.layout.fragment_audio_recorder, container, false);
+
+        // Set button onClick Handler
+        final ToggleButton toggle = (ToggleButton) view.findViewById(R.id.RecordingButton);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                onRecord(isChecked);
+                isChecked = !isChecked;
+            }
+        });
+        return view;
     }
+
+    private void onRecord(boolean start) {
+        if (start) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+
 }
