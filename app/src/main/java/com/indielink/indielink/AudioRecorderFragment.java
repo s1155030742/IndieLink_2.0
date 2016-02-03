5424b66ed459dfc9c67583b9cf2cc51f62a3a9a7
evanlee2048 <evanlee2048@gmail.com>
2016-02-03 18:17:21 +0800

    Trial for sound recording

--- app/src/main/java/com/indielink/indielink/AudioRecorderFragment.java
+++ app/src/main/java/com/indielink/indielink/AudioRecorderFragment.java
@@ -1,6 +1,9 @@
 package com.indielink.indielink;
 
+import android.content.Context;
+import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
@@ -10,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.ToggleButton;
 
 import java.io.IOException;
@@ -21,25 +25,17 @@ public class AudioRecorderFragment extends Fragment {
 
     private static final String LOG_TAG = "AudioRecordTest";
     private static String mFileName = null;
+
+    private RecordButton mRecordButton = null;
     private MediaRecorder mRecorder = null;
 
+    private PlayButton   mPlayButton = null;
+    private MediaPlayer mPlayer = null;
+
     public AudioRecorderFragment() {
-    }
+        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFileName += "/audiorecordtest.3gp";
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_audio_recorder, container, false);
-
-        // Set button onClick Handler
-        final ToggleButton toggle = (ToggleButton) view.findViewById(R.id.RecordingButton);
-        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                onRecord(isChecked);
-                isChecked = !isChecked;
-            }
-        });
-        return view;
     }
 
     private void onRecord(boolean start) {
@@ -48,6 +44,30 @@ public class AudioRecorderFragment extends Fragment {
         } else {
             stopRecording();
         }
+    }
+
+    private void onPlay(boolean start) {
+        if (start) {
+            startPlaying();
+        } else {
+            stopPlaying();
+        }
+    }
+
+    private void startPlaying() {
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(mFileName);
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
+
+    private void stopPlaying() {
+        mPlayer.release();
+        mPlayer = null;
     }
 
     private void startRecording() {
@@ -72,4 +92,84 @@ public class AudioRecorderFragment extends Fragment {
         mRecorder = null;
     }
 
+
+    class RecordButton extends Button {
+        boolean mStartRecording = true;
+
+        OnClickListener clicker = new OnClickListener() {
+            public void onClick(View v) {
+                onRecord(mStartRecording);
+                if (mStartRecording) {
+                    setText("Stop recording");
+                } else {
+                    setText("Start recording");
+                }
+                mStartRecording = !mStartRecording;
+            }
+        };
+
+        public RecordButton(Context ctx) {
+            super(ctx);
+            setText("Start recording");
+            setOnClickListener(clicker);
+        }
+    }
+
+    class PlayButton extends Button {
+        boolean mStartPlaying = true;
+
+        OnClickListener clicker = new OnClickListener() {
+            public void onClick(View v) {
+                onPlay(mStartPlaying);
+                if (mStartPlaying) {
+                    setText("Stop playing");
+                } else {
+                    setText("Start playing");
+                }
+                mStartPlaying = !mStartPlaying;
+            }
+        };
+
+        public PlayButton(Context ctx) {
+            super(ctx);
+            setText("Start playing");
+            setOnClickListener(clicker);
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        LinearLayout ll = new LinearLayout(getActivity());
+        mRecordButton = new RecordButton(getActivity());
+        ll.addView(mRecordButton,
+                new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        0));
+        mPlayButton = new PlayButton(getActivity());
+        ll.addView(mPlayButton,
+                new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        0));
+        getActivity().setContentView(ll);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_audio_recorder, container, false);
+
+        // Set button onClick Handler
+        final ToggleButton toggle = (ToggleButton) view.findViewById(R.id.RecordingButton);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                onRecord(isChecked);
+                isChecked = !isChecked;
+            }
+        });
+        return view;
+    }
 }
