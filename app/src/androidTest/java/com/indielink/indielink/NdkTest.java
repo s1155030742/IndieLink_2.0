a11ab68349bdb38bd417ff1543915d173abbd120
evanlee2048 <evanlee2048@gmail.com>
2016-04-05 19:05:48 +0800

    generate wav file for analysis now

--- app/src/androidTest/java/com/indielink/indielink/NdkTest.java
+++
@@ -1,8 +0,0 @@
-package com.indielink.indielink;
-
-/**
- * Created by indielink on 4/4/16.
- */
-public class NdkTest {
-
-}
--- app/src/main/java/com/indielink/indielink/SoundTrackFragment.java
+++ app/src/main/java/com/indielink/indielink/SoundTrackFragment.java
@@ -2,6 +2,8 @@ package com.indielink.indielink;
 
 import android.app.Activity;
 import android.content.Context;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
@@ -22,6 +24,9 @@ import com.indielink.indielink.CustomListener.OnSwipeTouchListener;
 import com.indielink.indielink.Profile.SoundTrackContent.SoundTrackItem;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,21 +38,31 @@ public class SoundTrackFragment extends Fragment {
     }
 
     private static final String LOG_TAG = "IndieLinkAudio";
-
     private File dir =null;
-
     private static String FilePath = null;
     private static String mFileName = null;
-
     private MediaRecorder mRecorder = null;
     private Audio audio = null;
-
     private static final String ARG_COLUMN_COUNT = "column-count";
     private int mColumnCount = 1;
-
     private String UserName;
-
     private MySoundTrackRecyclerViewAdapter mAdapter;
+
+    private Long mTsLong;
+
+    // 音頻獲取源
+    private int audioSource = MediaRecorder.AudioSource.MIC;
+    // 設置音頻采样率，44100是目前的標准，但是某些設備仍然支持22050，16000，11025
+    private static int sampleRateInHz = 44100;
+    // 設置音頻的錄制的聲道CHANNEL_IN_STEREO为雙聲道，CHANNEL_CONFIGURATION_MONO为單聲道
+    private static int channelConfig = AudioFormat.CHANNEL_IN_STEREO;
+    // 音頻數據格式:PCM 16位每個样本。保證設備支持。PCM 8位每個样本。不一定能得到設備支持。
+    private static int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+    // 緩沖區字節大小
+    private int bufferSizeInBytes = 0;
+    private AudioRecord audioRecord;
+
+    private boolean mIsRecord = false;
 
     public SoundTrackFragment() {
         FilePath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/IndieLinkAudio";
@@ -75,6 +90,13 @@ public class SoundTrackFragment extends Fragment {
             mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
             UserName = getArguments().getString("UserName");
         }
+
+        // 獲得緩沖區字節大小
+        bufferSizeInBytes = AudioRecord.getMinBufferSize(sampleRateInHz,
+                channelConfig, audioFormat);
+        // 創建AudioRecord對象
+        audioRecord = new AudioRecord(audioSource, sampleRateInHz,
+                channelConfig, audioFormat, bufferSizeInBytes);
     }
 
     @Override
@@ -84,20 +106,14 @@ public class SoundTrackFragment extends Fragment {
         //Set button onClick Handler
         final ToggleButton record = (ToggleButton) view.findViewById(R.id.RecordButton);
         record.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                            onRecord(isChecked);
-                            isChecked = !isChecked;
-                        }
-                    });
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                onRecord(isChecked);
+                mIsRecord = isChecked;
+                isChecked = !isChecked;
+            }
+        });
 
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
-
-        OnSwipeTouchListener mOnSwipe = new OnSwipeTouchListener(view.getContext()) {
-            public void onSwipeRight() {
-                //mAdapter.mValues
-                //mAdapter.notifyDataSetChanged();
-            }
-        };
 
         // Set the adapter
         if (recyclerView instanceof RecyclerView) {
@@ -113,6 +129,12 @@ public class SoundTrackFragment extends Fragment {
             recyclerView.setAdapter(mAdapter);
         }
         return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopRecording();
     }
 
     private List<SoundTrackItem> getListOfFile()
@@ -134,8 +156,8 @@ public class SoundTrackFragment extends Fragment {
         if (start) {
             File f = new File(FilePath);
             File file[] = f.listFiles();
-            Long tsLong = System.currentTimeMillis()/1000;
-            mFileName = FilePath + "/" + UserName + tsLong.toString() + ".3gp";
+            mTsLong = System.currentTimeMillis()/1000;
+            mFileName = FilePath + "/" + UserName + mTsLong.toString();
             startRecording();
         } else {
             stopRecording();
@@ -146,7 +168,7 @@ public class SoundTrackFragment extends Fragment {
         mRecorder = new MediaRecorder();
         mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        mRecorder.setOutputFile(mFileName);
+        mRecorder.setOutputFile(mFileName + ".3gp");
         mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 
         try {
@@ -155,6 +177,9 @@ public class SoundTrackFragment extends Fragment {
             Log.e(LOG_TAG, "prepare() failed");
         }
         mRecorder.start();
+
+        // 開启音頻文件寫入線程
+        new Thread(new AudioRecordThread()).start();
     }
 
     private void stopRecording() {
@@ -162,7 +187,144 @@ public class SoundTrackFragment extends Fragment {
         mRecorder.release();
         mRecorder = null;
         String file = mFileName.split(FilePath+"/")[1];
+
+        if (audioRecord != null) {
+            audioRecord.stop();
+            audioRecord.release();//釋放資源
+            audioRecord = null;
+        }
+
         mAdapter.mValues.add(new SoundTrackItem(String.valueOf(mAdapter.getItemCount() + 1),file ));
         mAdapter.notifyDataSetChanged();
+    }
+
+    class AudioRecordThread implements Runnable {
+        @Override
+        public void run() {
+            writeDateTOFile();//往文件中寫入裸數據
+            copyWaveFile(mFileName+".raw", mFileName+".wav");//给裸數據加上頭文件
+        }
+    }
+
+    /**
+     * 這裏將數據寫入文件，但是並不能播放，因为AudioRecord獲得的音頻是原始的裸音頻，
+     * 如果需要播放就必須加入一些格式或者編碼的頭信息。但是這样的好處就是你可以對音頻的 裸數據進行處理，比如你要做一個愛說話的TOM
+     * 貓在這裏就進行音頻的處理，然後重新封裝 所以說這样得到的音頻比較容易做一些音頻的處理。
+     */
+    private void writeDateTOFile() {
+        // new一個byte數組用來存一些字節數據，大小为緩沖區大小
+        byte[] audiodata = new byte[bufferSizeInBytes];
+        FileOutputStream fos = null;
+        int readsize = 0;
+        try {
+            File file = new File(mFileName+".raw");
+            if (file.exists()) {
+                file.delete();
+            }
+            fos = new FileOutputStream(file);// 建立一個可存取字節的文件
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        while (mIsRecord == true) {
+            readsize = audioRecord.read(audiodata, 0, bufferSizeInBytes);
+            if (AudioRecord.ERROR_INVALID_OPERATION != readsize) {
+                try {
+                    fos.write(audiodata);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        try {
+            fos.close();// 關閉寫入流
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 這裏得到可播放的音頻文件
+    private void copyWaveFile(String inFilename, String outFilename) {
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        long totalAudioLen = 0;
+        long totalDataLen = totalAudioLen + 36;
+        long longSampleRate = sampleRateInHz;
+        int channels = 2;
+        long byteRate = 16 * sampleRateInHz * channels / 8;
+        byte[] data = new byte[bufferSizeInBytes];
+        try {
+            in = new FileInputStream(inFilename);
+            out = new FileOutputStream(outFilename);
+            totalAudioLen = in.getChannel().size();
+            totalDataLen = totalAudioLen + 36;
+            WriteWaveFileHeader(out, totalAudioLen, totalDataLen,
+                    longSampleRate, channels, byteRate);
+            while (in.read(data) != -1) {
+                out.write(data);
+            }
+            in.close();
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 這裏提供一個頭信息。插入這些信息就可以得到可以播放的文件。
+     * 为我为啥插入這44個字節，這個還真沒深入研究，不過你隨便打開一個wav
+     * 音頻的文件，可以發現前面的頭文件可以說基本一样哦。每種格式的文件都有
+     * 自己特有的頭文件。
+     */
+    private void WriteWaveFileHeader(FileOutputStream out, long totalAudioLen,
+                                     long totalDataLen, long longSampleRate, int channels, long byteRate)
+            throws IOException {
+        byte[] header = new byte[44];
+        header[0] = 'R'; // RIFF/WAVE header
+        header[1] = 'I';
+        header[2] = 'F';
+        header[3] = 'F';
+        header[4] = (byte) (totalDataLen & 0xff);
+        header[5] = (byte) ((totalDataLen >> 8) & 0xff);
+        header[6] = (byte) ((totalDataLen >> 16) & 0xff);
+        header[7] = (byte) ((totalDataLen >> 24) & 0xff);
+        header[8] = 'W';
+        header[9] = 'A';
+        header[10] = 'V';
+        header[11] = 'E';
+        header[12] = 'f'; // 'fmt ' chunk
+        header[13] = 'm';
+        header[14] = 't';
+        header[15] = ' ';
+        header[16] = 16; // 4 bytes: size of 'fmt ' chunk
+        header[17] = 0;
+        header[18] = 0;
+        header[19] = 0;
+        header[20] = 1; // format = 1
+        header[21] = 0;
+        header[22] = (byte) channels;
+        header[23] = 0;
+        header[24] = (byte) (longSampleRate & 0xff);
+        header[25] = (byte) ((longSampleRate >> 8) & 0xff);
+        header[26] = (byte) ((longSampleRate >> 16) & 0xff);
+        header[27] = (byte) ((longSampleRate >> 24) & 0xff);
+        header[28] = (byte) (byteRate & 0xff);
+        header[29] = (byte) ((byteRate >> 8) & 0xff);
+        header[30] = (byte) ((byteRate >> 16) & 0xff);
+        header[31] = (byte) ((byteRate >> 24) & 0xff);
+        header[32] = (byte) (2 * 16 / 8); // block align
+        header[33] = 0;
+        header[34] = 16; // bits per sample
+        header[35] = 0;
+        header[36] = 'd';
+        header[37] = 'a';
+        header[38] = 't';
+        header[39] = 'a';
+        header[40] = (byte) (totalAudioLen & 0xff);
+        header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
+        header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
+        header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
+        out.write(header, 0, 44);
     }
 }
